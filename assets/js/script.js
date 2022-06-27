aa1f1e64b2cf9f33a1316263f4ad5fc6ce9c7885
TJP <tomi.palasi@gmail.com>
2022-06-27 16:33:51 -0300

    modificacion  script shop

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ const btnSwitch = document.querySelector(".darkmode-toggle");
 btnSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+//LOCAL STORAGE
+
+btnSwitch.onclick = checkDarkMode;
+
+function checkDarkMode(){
+
+}
