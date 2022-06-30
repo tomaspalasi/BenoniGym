daadbb696ee4730b1ae2275fd9bc1750eaf3630c
TJP <tomi.palasi@gmail.com>
2022-06-30 15:46:04 -0300

    agregado de operador ternario

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,9 @@ const btnSwitch = document.querySelector(".darkmode-toggle");
 
 btnSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark")
-  if (localStorage.getItem("dMode") == "False" || localStorage.getItem("dMode") == null){
-    localStorage.setItem("dMode", "True")
-  }
-  else {
-    localStorage.setItem("dMode", "False")
-  }
+    localStorage.getItem("dMode") == "False" || localStorage.getItem("dMode") == null ? 
+      localStorage.setItem("dMode", "True") :
+        localStorage.setItem("dMode", "False")
 })
 
 if (localStorage.getItem("dMode") == "True"){
@@ -18,13 +15,12 @@ if (localStorage.getItem("dMode") == "True"){
 
 //CARRITO DESDE INDEX
 
-let indice = ""
+let indice = "";
 
-if (window.location.href.endsWith("index.html")){
-  indice = "pages/"
-} else {
-  indice = ""
-}
+window.location.href.endsWith("index.html")?
+  indice = "pages/": 
+    indice = "";
+
 //CARRITO EMPTY BOTON
 
 const continuarCarrito = document.querySelector(".finalizarcompra");
@@ -32,17 +28,13 @@ const continuarCarrito = document.querySelector(".finalizarcompra");
 var carritoCheck = JSON.parse(localStorage.getItem("carrito"))
 
 if (window.location.href.endsWith("shop.html")){
-
   continuarCarrito.addEventListener("click", () => {
-  var carritoCheck = JSON.parse(localStorage.getItem("carrito"))
-    if (carritoCheck == null){
-      location.href="carritoEmpty.html"
-    } 
-    else {
-      location.href="carrito.html"
-    }
-  })
-}
+    var carritoCheck = JSON.parse(localStorage.getItem("carrito"))
+      carritoCheck == null ?
+        location.href="carritoEmpty.html":
+          location.href="carrito.html"
+      })
+  }
 
 // CARRITO EMPTY FOOTER Y NAV
 
@@ -51,13 +43,11 @@ const shopCart = document.querySelectorAll(".carritoVacio");
 for (let element of shopCart) {
   element.addEventListener("click", () => {
     var carritoCheck = JSON.parse(localStorage.getItem("carrito"))
-    if (carritoCheck == null){
-      location.href= indice + "carritoEmpty.html"
-    } 
-    else {
-      location.href= indice + "carrito.html"
+      carritoCheck == null ?
+        location.href= indice + "carritoEmpty.html":
+          location.href= indice + "carrito.html"
     }
-  })
+  )
 }
 
 
@@ -68,9 +58,7 @@ const carroVacio = document.querySelector("#carroVacio");
 
 carroVacio.addEventListener("click", () => {
   var carritoCheck = JSON.parse(localStorage.getItem("carrito"))
-  if (carritoCheck == null){
-    location.href= indice + "carritoEmpty.html"
-  } else {
-    location.href= indice + "carrito.html"
-  }
-})
+    carritoCheck == null?
+      location.href= indice + "carritoEmpty.html":
+        location.href= indice + "carrito.html"
+    })
